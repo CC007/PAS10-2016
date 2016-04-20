d75ce56106082e9680b145bf4c63232f1d4ec79d
Student Account <Student@example.com>
2016-04-20 16:17:37 +0200

    Tutorial until 1.4 Using rostopic pub

--- rikfiles/catkin_ws/build/catkin_generated/order_packages.py
+++ rikfiles/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/student/catkin_ws/rikfiles/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/student/sudo/ros/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/student/sudo/ros/catkin_ws/devel;/opt/ros/indigo" != "" else []
+underlay_workspaces = "/home/student/catkin_ws/rikfiles/catkin_ws/devel;/home/student/sudo/ros/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/student/catkin_ws/rikfiles/catkin_ws/devel;/home/student/sudo/ros/catkin_ws/devel;/opt/ros/indigo" != "" else []
--- rikfiles/catkin_ws/build/catkin_generated/setup_cached.sh
+++ rikfiles/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,12 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/student/catkin_ws/rikfiles/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export CPATH="/home/student/catkin_ws/rikfiles/catkin_ws/devel/include:$CPATH"
-export LD_LIBRARY_PATH="/home/student/catkin_ws/rikfiles/catkin_ws/devel/lib:/home/student/catkin_ws/rikfiles/catkin_ws/devel/lib/x86_64-linux-gnu:/home/student/sudo/ros/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/student/sudo/ros/catkin_ws/devel/lib:/opt/ros/indigo/lib:/usr/local/lib:/usr/local/cuda/lib64"
-export PATH="/home/student/catkin_ws/rikfiles/catkin_ws/devel/bin:$PATH"
-export PKG_CONFIG_PATH="/home/student/catkin_ws/rikfiles/catkin_ws/devel/lib/pkgconfig:/home/student/catkin_ws/rikfiles/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/home/student/sudo/ros/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/student/sudo/ros/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/student/catkin_ws/rikfiles/catkin_ws/build"
-export PYTHONPATH="/home/student/catkin_ws/rikfiles/catkin_ws/devel/lib/python2.7/dist-packages:/home/student/sudo/ros/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:/home/student/sudo/brain/src/vision/autoencoder:/home/student/sudo/brain/src"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/student/catkin_ws/rikfiles/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/student/catkin_ws/rikfiles/catkin_ws/src:/home/student/sudo/ros/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
